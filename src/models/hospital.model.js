458548973d1ee761abf160e1a47b964154de34b8
thiru1832 <rrthirumugilan18@gmail.com>
2023-05-31 19:13:14 +0530

    changes hospital model

--- src/models/hospital.model.js
+++ src/models/hospital.model.js
@@ -1,10 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const hospitalSchema = new mongoose.Schema({
-    hospitalName: String,
-    email: String
+  hospitalRegNo: String,
+  hospitalName: String,
+  email: String,
+  fax: String,
+  contactNo: Number,
+  country: String,
+  state: String,
+  city: String,
+  pincode: String,
+  address: String,
+  password: String,
+  planType: String,
 });
 
-const Hospital = mongoose.model('hospitals', hospitalSchema);
+const Hospital = mongoose.model("hospitals", hospitalSchema);
 
 module.exports = Hospital;
